0afa2aa84cdbf84a5802fc4a74086fd177dbfc6a
Unknown <supakorn.trakulmaiphol@mail.kmutt.ac.th>
2019-04-27 21:10:57 +0700

    [ADD] Backend เชื่อมต่อกับ  Elasticsearch บน Cloud

--- WatchmeBackend/src/main/java/com/dreamteam/watchme/service/TrackingStatusService.java
+++ WatchmeBackend/src/main/java/com/dreamteam/watchme/service/TrackingStatusService.java
@@ -5,12 +5,30 @@
  */
 package com.dreamteam.watchme.service;
 
+import com.dreamteam.watchme.config.ElasticUtil;
 import com.dreamteam.watchme.model.TrackingStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.dreamteam.watchme.repository.TrackingStatusRepository;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.ws.http.HTTPException;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.http.HttpStatus;
 
 /**
@@ -19,6 +37,11 @@ import org.springframework.http.HttpStatus;
  */
 @Service
 public class TrackingStatusService {
+
+    @Autowired
+    private RestHighLevelClient elasticClient;
+
+    private final String index = "tracking_status";
 
     @Autowired
     private TrackingStatusRepository trackingStatusRepository;
@@ -32,6 +55,7 @@ public class TrackingStatusService {
     public TrackingStatus updateUserTrackingStatus(TrackingStatus trackingStatus) {
         TrackingStatus updatedTrackingStatus = null;
         try {
+            
             updatedTrackingStatus = trackingStatusRepository.save(trackingStatus);
             return trackingStatus;
         } catch (Exception e) {
@@ -44,6 +68,63 @@ public class TrackingStatusService {
 
     public List<TrackingStatus> getAllUserTrackingDetail() {
         return trackingStatusRepository.findAll();
+    }
+    
+
+    public List<TrackingStatus> findTrackingStatusByUsingFilter(String[] eventTags, boolean isRecently, String eventDetail) throws IOException {
+        BoolQueryBuilder queryFilter = new BoolQueryBuilder();
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        SearchRequest searchRequest = new SearchRequest(index);
+        SearchResponse searchResponse = null;
+
+        System.out.println("--- Start Filtering ---");
+        if (eventTags != null) {
+            System.out.println("Event Tag");
+            queryFilter = filterByTrackingStatusTags(queryFilter, eventTags);
+        }
+        if (eventDetail != null) {
+            System.out.println("TrackingStatus Detail");
+            queryFilter.must(filterByTrackingStatusDetail(eventDetail));
+        }
+        
+        if (isRecently == true) {
+            System.out.println("Recently");
+            searchSourceBuilder = filterByRecently(searchSourceBuilder, "createEventDate");
+        }
+
+        searchSourceBuilder.query(queryFilter);
+        searchRequest.source(searchSourceBuilder);
+        searchResponse = elasticClient.search(searchRequest, RequestOptions.DEFAULT);
+
+        return ElasticUtil.searchHitsToList(searchResponse.getHits(), TrackingStatus.class);
+    }
+
+    public List<TrackingStatus> findAllTrackingStatusInElastic() throws IOException {
+        System.out.println("find all event");
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices(index);
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = elasticClient.search(searchRequest, RequestOptions.DEFAULT);
+
+        return ElasticUtil.searchHitsToList(searchResponse.getHits(), TrackingStatus.class);
+    }
+
+    public BoolQueryBuilder filterByTrackingStatusTags(BoolQueryBuilder queryFilter, String eventTags[]) {
+        queryFilter.filter(QueryBuilders.termsQuery("eventTags", eventTags));
+        return queryFilter;
+    }
+
+    public MatchQueryBuilder filterByTrackingStatusDetail(String eventDetail) {
+        MatchQueryBuilder alreadyFilterByEventDetail = QueryBuilders.matchQuery("eventDetail", eventDetail);
+        return alreadyFilterByEventDetail;
+    }
+
+    public SearchSourceBuilder filterByRecently(SearchSourceBuilder searchSourceBuilder, String sortedField) throws IOException {
+        searchSourceBuilder.sort(sortedField, SortOrder.DESC);
+        return searchSourceBuilder;
     }
 
 }
