bb960b708c00778f49c6d4a857557b9dc735bc99
bigzaja4 <bigzaja4@hotmail.com>
2019-04-28 02:05:39 +0700

    [UPDATE] format home and chart

--- watch-me/src/views/Home.vue
+++ watch-me/src/views/Home.vue
@@ -1,19 +1,27 @@
 <template>
-    <v-layout row wrap justify-center>
-      <v-flex xs12>
-        <v-data-table
-    :headers="headers"
-    :items="status"
-    class="elevation-1"
-    
-  >
-    <template v-slot:items="props" >
-        <tr @click="showChart(props.item.trackingStatusId,props.item.status)">
-      <td class="text-xs-left">{{ props.item.username }}</td>
-      <td class="text-xs-left">{{ props.item.heartRate}}</td>
-      <td class="text-xs-left"><v-chip :color="getStatusColor(props.item.status)" text-color="white" > {{ props.item.status }} </v-chip> </td>
-      <td class="text-xs-left"><v-btn small :to="{name: 'map' , props: {trackingId: props.item.trackingStatusId}}" >Location</v-btn></td>
-        </tr>
+  <v-layout row wrap justify-center>
+    <v-flex xs12>
+      <v-data-table :headers="headers" :items="status" class="elevation-1">
+        <template v-slot:items="props">
+          <tr @click="showChart(props.item.trackingStatusId,props.item.status)">
+            <td class="text-xs-left">{{ props.item.username }}</td>
+            <td class="text-xs-left">{{ props.item.heartRate}}</td>
+            <td class="text-xs-left">
+              <v-chip
+                :color="getStatusColor(props.item.status)"
+                text-color="white"
+              >{{ props.item.status }}</v-chip>
+            </td>
+            <td class="text-xs-left">
+              <v-btn
+                small
+                :to="{name: 'map' , props: {trackingId: props.item.trackingStatusId}}"
+              >Location</v-btn>
+            </td>
+          </tr>
+        </template>
+      </v-data-table>
+    </v-flex>
 
     <v-flex xs9 :key="idClicked" v-if="isShowChart">
       <chartInfo :status="statusId"/>
@@ -24,6 +32,7 @@
 <script>
 import chartInfo from "../components/Chart";
 import { mapGetters } from "vuex";
+import { setInterval } from "timers";
 export default {
   components: {
     chartInfo
@@ -40,44 +49,33 @@ export default {
           sortable: false,
           value: "username"
         },
-          { text: 'Heart Rate', value: 'heartRate' },
-          { text: 'Status', value: 'status' },
-          { text: 'Location', value: 'location' },
-        ],
-         
+        { text: "Heart Rate", value: "heartRate" },
+        { text: "Status", value: "status" },
+        { text: "Location", value: "location" }
+      ]
+    };
+  },
+  methods: {
+    showChart: function(trackId, status) {
+      if (this.idClicked == trackId) {
+        this.idClicked = 0;
+        this.isShowChart = false;
+      } else {
+        this.idClicked = trackId;
+        this.isShowChart = true;
       }
-    },
-    methods: {
-        showChart: function(trackId,status){            
-            if(this.idClicked == trackId){
-                this.idClicked = 0;
-                this.isShowChart = false;
-            }
-            else{
-                this.idClicked = trackId;
-                this.isShowChart = true;
-            }
-
-            if(status == 'normal'){
-                this.statusId = 0;
-            }
-            else{
-                this.statusId = 1;
-            }
-        },
-        getStatusColor: function(status) {
-            if(status == 'normal'){
-              return 'primary';
-            }
-            else{
-              return 'red';
-            }
-        }
 
       if (status == "normal") {
         this.statusId = 0;
       } else {
         this.statusId = 1;
+      }
+    },
+    getStatusColor: function(status) {
+      if (status == "normal") {
+        return "primary";
+      } else {
+        return "red";
       }
     }
   },
