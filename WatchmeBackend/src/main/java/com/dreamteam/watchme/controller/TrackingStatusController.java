46abc2b0cf8e655586d0d244a3e1ddd44f2c151e
Unknown <supakorn.trakulmaiphol@mail.kmutt.ac.th>
2019-04-28 04:01:30 +0700

    [ADD] CORS

--- WatchmeBackend/src/main/java/com/dreamteam/watchme/controller/TrackingStatusController.java
+++ WatchmeBackend/src/main/java/com/dreamteam/watchme/controller/TrackingStatusController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author wdrdr
  */
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 @RestController
 public class TrackingStatusController {
 
